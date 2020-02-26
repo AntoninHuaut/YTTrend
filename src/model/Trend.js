fdea65542f43965c57653422ab9d79d8493b29f2
Maner <antonin.huaut+github@gmail.com>
2020-02-26 11:21:24 +0100

    24 hour format

--- src/model/Trend.js
+++ src/model/Trend.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 
 module.exports = class Trend {
     constructor(date, videos) {
-        this.date = moment(date).format('YYYY-MM-DD hh:mm:ss');
+        this.date = moment(date).format('YYYY-MM-DD HH:mm:ss');
         this.videos = videos;
     }
 }
