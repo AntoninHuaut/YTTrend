5e2848063735773e85d9cd7c823d49728dc774f7
Maner <antonin.huaut+github@gmail.com>
2020-02-27 16:54:12 +0100

    Change tagList

--- src/sql/api.js
+++ src/sql/api.js
@@ -35,13 +35,17 @@ function groupByVideo(result) {
 function handleVideo(res, video) {
     const filtered = res.filter(resVid => resVid.videoId === video.videoId);
     if (filtered.length) {
-        filtered[0].tagsId.push(video.tagId);
-        filtered[0].tagsName.push(video.tagName);
+        filtered[0].tags.push({
+            tagId: video.tagId,
+            tagName: video.tagName
+        });
     } else {
-        video.tagsId = [];
-        video.tagsName = [];
-        video.tagsId.push(video.tagId);
-        video.tagsName.push(video.tagName);
+        video.tags = [];
+        if (video.tagId) video.tags.push({
+            tagId: video.tagId,
+            tagName: video.tagName
+        });
+
         delete video.tagId;
         delete video.tagName;
         res.push(video);
